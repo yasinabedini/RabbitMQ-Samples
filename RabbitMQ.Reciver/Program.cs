3b707ac542abbdba5c4a72400cd5670d31f4d80c
yasin abedini <yasinabedini1384@gmail.com>
2024-03-01 00:04:26 +0330

    Add project files.

---
+++ RabbitMQ.Reciver/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+
+string queueName = "testRabbitMQ";
+channel.QueueDeclare(queueName, false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += Consumer_Recived;
+
+channel.BasicConsume(queueName, true, consumer);
+
+Console.WriteLine("Press Key To Exit.....");
+Console.ReadLine();
+
+void Consumer_Recived(object? sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.ToArray();
+    var stringMessage = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message Recived : {stringMessage}");
+}
