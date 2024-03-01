5371f444a2d17c8b84a01f98d3d525431926881e
yasin abedini <yasinabedini1384@gmail.com>
2024-03-01 14:40:35 +0330

    feat - Add pub-sub rabbitMQ

---
+++ PubSub/PubSub.Sender/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string exchangeName = "FanoutMailBoxExchange";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, false);
+
+Console.Write("Enter Your Message : ");
+string message = Console.ReadLine() ?? "Default message";
+
+var messageByte = System.Text.Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchangeName, "", null, messageByte);
+Console.WriteLine("Your Message Sent");
+Console.ReadKey();
